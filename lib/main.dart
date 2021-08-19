052d1b4775b57e0a8d4e354b41348084ef7a3ac8
laksh97 <lakshlj816@gmail.com>
2021-08-19 15:33:35 +0530

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class _MyAppState extends State<MyApp> {
   TextEditingController _user = TextEditingController();
   TextEditingController _password = TextEditingController();
 
+  List<String> _pets = ["Dogs, Cats, Tigers, Lions"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,88 +29,95 @@ class _MyAppState extends State<MyApp> {
         title: Text("Flutter Layouts"),
         backgroundColor: Colors.blue,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Center(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text("Username"),
-                    SizedBox(
-                      height: 10,
-                      width: 10,
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text("Username"),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _user,
                     ),
-                    Expanded(
-                      child: TextField(
-                        controller: _user,
-                      ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Password"),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _password,
+                      obscureText: true,
                     ),
-                  ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () => print("Login ${_user.text}"),
+                  child: Text("Login"),
                 ),
-                Row(
-                  children: [
-                    Text("Password"),
-                    SizedBox(
-                      height: 10,
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        controller: _password,
-                        obscureText: true,
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
+              ),
+              Card(
+                elevation: 5,
+                shadowColor: Colors.blue,
+                child: Container(
                   padding: EdgeInsets.all(20),
-                  child: ElevatedButton(
-                    onPressed: () => print("Login ${_user.text}"),
-                    child: Text("Login"),
+                  child: Column(
+                    children: [
+                      Text("Hi Jay"),
+                      Text("Do you love Flutter?"),
+                    ],
                   ),
                 ),
-                Card(
-                  elevation: 5,
-                  shadowColor: Colors.blue,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Text("Hi Jay"),
-                        Text("Do you love Flutter?"),
-                      ],
-                    ),
+              ),
+              Card(
+                elevation: 5,
+                shadowColor: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text("Hi Sam"),
+                      Text("Do you love Flutter?"),
+                    ],
                   ),
                 ),
-                Card(
-                  elevation: 5,
-                  shadowColor: Colors.blue,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Text("Hi Sam"),
-                        Text("Do you love Flutter?"),
-                      ],
-                    ),
+              ),
+              Card(
+                elevation: 5,
+                shadowColor: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text("Hi Nav"),
+                      Text("Do you love Flutter?"),
+                    ],
                   ),
                 ),
-                Card(
-                  elevation: 5,
-                  shadowColor: Colors.blue,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Text("Hi Nav"),
-                        Text("Do you love Flutter?"),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _pets.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(
+                        _pets.elementAt(index),
+                      );
+                    }),
+              ),
+            ],
           ),
         ),
       ),
