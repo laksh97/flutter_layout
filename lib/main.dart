b31a526726c4ddb0daec028794af61b9dca51f92
laksh97 <lakshlj816@gmail.com>
2021-08-19 14:58:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+      title: "Flutter Notifications",
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController _user = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Layouts"),
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("Username"),
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: _user,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Password"),
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: _password,
+                        obscureText: true,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    onPressed: () => print("Login ${_user.text}"),
+                    child: Text("Login"),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  shadowColor: Colors.blue,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text("Hi Jay"),
+                        Text("Do you love Flutter?"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  shadowColor: Colors.blue,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text("Hi Sam"),
+                        Text("Do you love Flutter?"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  shadowColor: Colors.blue,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text("Hi Nav"),
+                        Text("Do you love Flutter?"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
